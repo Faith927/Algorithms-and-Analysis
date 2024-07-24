d7116fc22056e47387ed96c63792efec59b5a8ac
Faith927 <90170811+Faith927@users.noreply.github.com>
2024-07-24 14:07:56 +1000

    Update arraySpreadsheet.py

--- arraySpreadsheet.py
+++ arraySpreadsheet.py
@@ -1,7 +1,5 @@
 from spreadsheet.cell import Cell
 from spreadsheet.baseSpreadsheet import BaseSpreadsheet
-import numpy as np
-import sys
 import time
 
 # ------------------------------------------------------------------------
@@ -14,27 +12,29 @@ import time
 
 class ArraySpreadsheet(BaseSpreadsheet):
 
+
     def __init__(self):
-        # TO BE IMPLEMENTED
-        pass
+        self.table = []  # initializing the class
 
     def buildSpreadsheet(self, lCells: [Cell]):
-    # def buildSpreadsheet(self):
 
         """
         Construct the data structure to store nodes.
         @param lCells: list of cells to be stored
         """
-        print(lCells)
+        # finding the max number of rows and columns
+        max_row = max([item.row for item in lCells])
+        max_col = max([item.col for item in lCells])
 
-        # TO BE IMPLEMENTED
-        return self
+        # creating a 2D array with None as the values
+        table = [[None] * (max_col + 1) for _ in range(max_row + 1)]
 
-    array = [[11, 12, 5, 2, 9, 10, 11], [-6.7, 6, 10], [10, 8, 12, 5], [12, 5, 8, 1]]
-    spreadsheet = buildSpreadsheet(array, Cell)
-    # print(spreadsheet)
-    # spreadsheet.append([])
-    # print(spreadsheet)
+        # adding values into the 2D array if the index and column number match up
+        for item in lCells:
+            table[item.row][item.col] = item.val
+        self.table = table  # initializing the table
+
+
 
     def appendRow(self)->bool:
         """
@@ -43,26 +43,14 @@ class ArraySpreadsheet(BaseSpreadsheet):
         @return True if operation was successful, or False if not.
         """
 
-        # TO BE IMPLEMENTED
-        print(self)
-        # x = self.append([])
-        # print(x)
-        # # len(x)
-        # if len(x) > len(self):
-        #     print(True)
-        # else:
-        #     print(False)
-
         try:
-            print(True)
-            return self.append([])
-        except:
-            print(False)
-
-        # # REPLACE WITH APPROPRIATE RETURN VALUE
-        # return True
-    row = appendRow(spreadsheet)
-    # print(spreadsheet)
+            # appends an empty row to the bottom of the spreadsheet
+            max_len = len(self.table[0])
+            self.table.append([None] * max_len)
+            return True
+        except NameError:
+            # if table cannot be found, returns False
+            return False
 
 
     def appendCol(self)->bool:
@@ -71,18 +59,13 @@ class ArraySpreadsheet(BaseSpreadsheet):
 
         @return True if operation was successful, or False if not.
         """
-
-        # TO BE IMPLEMENTED
         try:
-            print(True)
-            return self[0].append(None)
+            # appends None to the end of each individual list
+            for elements in self.table:
+                elements.append(None)
+            return True
         except:
-            print(False)
-
-        # REPLACE WITH APPROPRIATE RETURN VALUE
-        # return new_col
-    col = appendCol(spreadsheet)
-    # print(spreadsheet)
+            return False
 
     def insertRow(self, rowIndex: int) -> bool:
         """
@@ -92,24 +75,19 @@ class ArraySpreadsheet(BaseSpreadsheet):
 
         @return True if operation was successful, or False if not, e.g., rowIndex is invalid.
         """
-        if rowIndex <= len(self) and rowIndex >= 0:
-            print(True)
-            return self.insert(rowIndex -1, [])
+
+        # makes sure rowIndex is valid and appends a list with the length of the biggest row
+        if 0 <= rowIndex <= len(self.table):
+            max_len = len(self.table[0])
+            self.table.insert(rowIndex, [None] * max_len)
+            return True
+        # appends row to the end of each list
+        elif rowIndex == -1:
+            max_len = len(self.table[0])
+            self.table.insert(-1, [None] * max_len)
         else:
-            print(False)
+            return False
 
-        # TO BE IMPLEMENTED
-        # try:
-        #     print(True)
-        #     return self.insert(rowIndex, [])
-        # except:
-        #     print(False)
-
-        # # REPLACE WITH APPROPRIATE RETURN VALUE
-        # return True
-
-    e_row = insertRow(spreadsheet, 2)
-    # print(spreadsheet)
 
     def insertCol(self, colIndex: int) -> bool:
         """
@@ -119,15 +97,21 @@ class ArraySpreadsheet(BaseSpreadsheet):
 
         return True if operation was successful, or False if not, e.g., colIndex is invalid.
         """
-        if colIndex <= len(self) and colIndex >= 0:
-            print(True)
-            return self[0].insert(colIndex -1, None)
+        # inserts None in the colIndex position for each list
+        num = []
+        for item in self.table:
+            num.append(len(item))
+        n = max(num)
+        if n >= colIndex >= 0:  # making sure the colIndex is a valid number
+            for element in self.table:
+                element.insert(colIndex, None)
+            return True
+        elif colIndex == -1:          # inserts None at the end of each list
+            for element in self.table:
+                element.insert(-1, None)
         else:
-            print(False)
+            return False
 
-
-    insert_col = insertCol(spreadsheet, 3)
-    # print(spreadsheet)
 
     def update(self, rowIndex: int, colIndex: int, value: float) -> bool:
         """
@@ -140,51 +124,25 @@ class ArraySpreadsheet(BaseSpreadsheet):
         @return True if cell can be updated.  False if cannot, e.g., row or column indices do not exist.
         """
 
-        # TO BE IMPLEMENTED
+        # uses the rowIndex and colIndex to find and replace the value
         try:
-            update_val = self[rowIndex][colIndex] = value
-            print(True)
-
-
+            self.table[rowIndex][colIndex] = value
+            return True
         except:
-            print(False)
-
-        # REPLACE WITH APPROPRIATE RETURN VALUE
-        return self
-    print(spreadsheet)
-
-    try:
-        col_ind = int(input("What is the column index? "))
-        row_ind = int(input("What is the row index? "))
-        new_value = float(input("what would you like to replace it with? "))
-        update_val = update(spreadsheet, row_ind, col_ind, new_value)
-        print(spreadsheet)
-    except:
-        print("wrong value inputted, please input only integers")
-
-
-
+            return False
 
     def rowNum(self)->int:
         """
         @return Number of rows the spreadsheet has.
         """
-        return len(self)
-
-    print("Number of rows for the spreadsheet: ", rowNum(spreadsheet))
-
+        return len(self.table)  # returning the number of lists in the 2D array
 
     def colNum(self)->int:
         """
         @return Number of column the spreadsheet has.
         """
-
-        list_len = [len(i) for i in self]
-        return max(list_len)
-
-    print("Number of columns for the spreadsheet: ", colNum(spreadsheet))
-
-
+        list_len = [len(i) for i in self.table]  # finding the length of the elements in the 2D array
+        return max(list_len)  # returning the max number found
 
     def find(self, value: float) -> [(int, int)]:
         """
@@ -194,42 +152,23 @@ class ArraySpreadsheet(BaseSpreadsheet):
 
         @return List of cells (row, col) that contains the input value.
 	    """
-
-        # TO BE IMPLEMENTED
-        from collections import defaultdict
-        elements = defaultdict(list)
-        for row_index in range(len(self)):
-            for col_index in range(len(self[row_index])):
-                elements[self[row_index][col_index]].append([row_index, col_index])
-
-        multiples = [elements[i] for i in elements if len(elements[i]) > 1 and i == value]
-        print(multiples)
-
-        # REPLACE WITH APPROPRIATE RETURN VALUE
-        return
-
-    find(spreadsheet, 12)
-
-
-
-
-
+        val_list = []
+        # enumerating through the 2D array to find the row index and the row values
+        for r_inx, row in enumerate(self.table):
+            for c_inx, col in enumerate(row):
+                if col == value:
+                    print(r_inx, c_inx)
+                    val_list.append((r_inx, c_inx))  # appending row index and column index to val_list
+        return val_list
 
     def entries(self) -> [Cell]:
         """
         @return A list of cells that have values (i.e., all non None cells).
         """
-
-        # TO BE IMPLEMENTED
-        pass
-
-        # TO BE IMPLEMENTED
-        return []
-
-
-    # function update
-    # col_ind= int(input("What is the column index? "))
-    # row_ind = int(input("What is the row index? "))
-    # new_value = float(input("what would you like to replace it with? "))
-    #
-    # update_val = update(insert_col, row_ind, col_ind, new_value)
+        values = []
+        # appends row and column index if value found
+        for r_inx, row in enumerate(self.table):
+            for c_inx, col in enumerate(row):
+                if col != None:
+                    values.append((r_inx, c_inx, col))  # appending row and column index to values
+        return values
